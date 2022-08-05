6a216682bccff0a3132080848e8942e214c10cad
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-08-05 12:01:30 -0400

    create initialize method and begin parent=

--- polytreenode/lib/00_tree_node.rb
+++ polytreenode/lib/00_tree_node.rb
@@ -1,3 +1,22 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    attr_writer :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
 
+    def parent=(daddy)
+        if daddy && @parent
+            @parent.children.delete(self)
+        elsif daddy.nil? && @parent
+            @parent.children.delete(self)
+        elsif !daddy.children.include?(self)
+            daddy.children << self
+        end
+
+
+        @parent = daddy
+    end
 end
